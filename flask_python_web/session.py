46b991a432c29964649fd3ac070a9c4e1ef4a398
kiendt <kiendt2312.work@gmail.com>
2026-05-11 22:46:31 +0700

    flaskbasic

--- flask_python_web/session.py
+++ flask_python_web/session.py
@@ -1,9 +1,24 @@
 from flask import Flask, redirect, session ,url_for, render_template, request, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAIchemy
+from os import path
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "kiendz"
-app.permanent_session_lifetime = timedelta(minutes=1)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+app.permanent_session_lifetime = timedelta(minutes=10)
+
+db = SQLAlchemy(app)
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 @app.route("/home")
 @app.route("/")
@@ -14,20 +29,19 @@ def home():
 def login():
     if request.method == "POST":
         user_name = request.form["name"]
+        user_email = request.form["email"]
         session.permanent = True
         if user_name:
             session["user"] = user_name
-            flash("You are logged in successfully! ", "info")
-            return render_template("user.html", user=user_name)
+            return redirect(url_for("user.html", user=user_name))
         if "user" in session:
             name = session["user"]
-            flash("You have already logged in! ", "info")
-            return render_template("user.html", user=name)
+            return redirect(url_for("user.html", user=user_name))
     return render_template("login.html")
 
 
 @app.route("/user")
-def hello_user():
+def user():
     if "user" in session:
         name = session["user"]
         return render_template("user.html", user=name)
@@ -43,4 +57,7 @@ def log_out():
     return redirect(url_for("login"))
 
 if __name__=="__main__":
+    if not path.exists("users.db"):
+        db.create_all()
     app.run(debug=True)
+    
